8b0dc1998cfcbd15c253c88e92abf0f33830d7b9
Vahram Jacobs <vahram.jacobs@gmail.com>
2020-01-02 08:43:58 -0800

    Changes app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>baby gambiono</p>
+				<p>I've got changed on the feature branch.</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
